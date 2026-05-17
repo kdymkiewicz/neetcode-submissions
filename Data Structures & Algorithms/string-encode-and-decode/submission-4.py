b33c850219704ee48e882660f055f7bc2f969d5d
Kajetan Dymkiewicz <kajetan.dymkiewicz@gmail.com>
2026-05-17 19:23:16 +0100

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,39 @@
+def get_n(s):
+    ri = s.find(">")
+
+    n = s[1:ri]
+    print("found number:" + n)
+    return int(n), len(n)+2
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        e_str = ""
+
+        for s in strs:
+            n = len(s)
+            e_str += "<"+str(n)+">"+s
+
+        return e_str
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        print(s)
+        if s:
+            n,offset = get_n(s)
+
+            print(n)
+            print(offset)
+
+            decoded.append(s[offset:n+offset])
+
+            rem = s[n+offset:]
+
+            while len(rem) > 0:
+                n,offset = get_n(rem)
+                decoded.append(rem[offset:n+offset])
+
+                rem = rem[n+offset:]
+        
+        return decoded
